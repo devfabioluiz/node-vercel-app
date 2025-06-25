f4e389537b53a983cfe33f83bd6e9e6e5c687e8e
Fabio Luiz Daemon Da Silva <TR044710@comgas.com.br>
2025-06-25 16:00:01 -0300

    initial commit

---
+++ api/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello, Vercel!" });
+});
+
+module.exports = app;
